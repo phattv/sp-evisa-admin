4c6deba70f5aeb494311dee1495f1e874db3b974
Phat Tran <PHAT@Phattv.local>
2018-03-28 09:50:04 +0700

    Follow admin-on-rest tutorial (except custom restClient) https://marmelab.com/admin-on-rest//Tutorial.html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Admin, Delete, jsonServerRestClient, Resource } from 'admin-on-rest';
+import PostIcon from 'material-ui/svg-icons/action/book';
+import UserIcon from 'material-ui/svg-icons/social/group';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { PostCreate, PostEdit, PostList } from './posts';
+import { UserList } from './users';
+import Dashboard from './Dashboard';
+import authClient from './authClient';
+
+const App = () => (
+  <Admin authClient={authClient} dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
+);
 
 export default App;
