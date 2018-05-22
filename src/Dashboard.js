d489048cbfe93012178b26035a6c16dddd85d42b
Phat Tran <PHAT@Phattv.local>
2018-05-22 16:22:42 +0700

    Move legend into dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,5 +5,12 @@ export default () => (
   <Card style={{ margin: '2em' }}>
     <CardHeader title="Welcome to the administration" />
     <CardText>Admin Panel for evisa-vn.com</CardText>
+    <CardText>
+      Legend:
+      <p>1MS: 1 Month Single - 1MM: 1 Month Multiple</p>
+      <p>3MS: 3 Month Single - 3MM: 3 Month Multiple</p>
+      <p>6MM: 6 Month Multiple</p>
+      <p>1YM: 1 Year Multiple</p>
+    </CardText>
   </Card>
 );
--- src/fees.js
+++ src/fees.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {
+  Card,
+  CardHeader,
+  CardText,
   Create,
   Datagrid,
   DisabledInput,
@@ -35,32 +38,22 @@ const FeeFilter = props => (
 );
 
 const FeeList = props => (
-  <div>
-    Legend:
-    <p>1MS: 1 Month Single - 1MM: 1 Month Multiple</p>
-    <p>3MS: 3 Month Single - 3MM: 3 Month Multiple</p>
-    <p>6MM: 6 Month Multiple - 1YM: 1 Year Multiple</p>
-    <List {...props} filters={<FeeFilter />} perPage={constants.pageSize}>
-      <Datagrid>
-        <NumberField source="id" />
-        <ReferenceField
-          label="Country"
-          source="country_id"
-          reference="countries"
-        >
-          <TextField source="name" />
-        </ReferenceField>
-        <NumberField source="type" />
-        <NumberField source="one_month_single" label="1MS" />
-        <NumberField source="one_month_multiple" label="1MM" />
-        <NumberField source="three_month_single" label="3MS" />
-        <NumberField source="three_month_multiple" label="3MM" />
-        <NumberField source="six_month_multiple" label="6MM" />
-        <NumberField source="one_year_multiple" label="1YM" />
-        <EditButton />
-      </Datagrid>
-    </List>
-  </div>
+  <List {...props} filters={<FeeFilter />} perPage={constants.pageSize}>
+    <Datagrid>
+      <NumberField source="id" />
+      <ReferenceField label="Country" source="country_id" reference="countries">
+        <TextField source="name" />
+      </ReferenceField>
+      <NumberField source="type" />
+      <NumberField source="one_month_single" label="1MS" />
+      <NumberField source="one_month_multiple" label="1MM" />
+      <NumberField source="three_month_single" label="3MS" />
+      <NumberField source="three_month_multiple" label="3MM" />
+      <NumberField source="six_month_multiple" label="6MM" />
+      <NumberField source="one_year_multiple" label="1YM" />
+      <EditButton />
+    </Datagrid>
+  </List>
 );
 
 const FeeShow = props => (
